c165c889090ded337880016a7bb088f72ba76387
Laurent <lau.cazanove@gmail.com>
2017-02-08 22:15:21 +0100

    Added API routes that return JSON

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*
  * Modules
  */
+
 var express	= require('express');
 var fs 		= require('fs');
 var path 	= require('path');
@@ -8,23 +9,35 @@ var path 	= require('path');
 /*
  * Data
  */
+
 const data = JSON.parse(fs.readFileSync('DATA.json'));
 
 /*
  * Application
  */
+
 var app = express();
 app.use(express.static('public'));
 
-// Index
+/* Web routes */
+
 app.get('/', function(req, res) {
 	res.send('Hello world');
 });
 
-// Home page
 app.get('/home', function(req, res) {
 	res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
+
+/* Api routes */
+
+app.get('/data/champions', function(req, res) {
+	res.json(data.champions);
+});
+
+app.get('/data/matches', function(req, res) {
+	res.json(data.matches);
+})
 
 // Start server
 app.listen(2020, function() {
--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,27 @@
 var app = new Vue({
+
 	el: '#app',
+
 	data: {
-		message: 'Hello Vue!'
+		champions: {}
+	},
+
+	// When application is ready
+	mounted: function () {
+		this.$nextTick(function () {
+			this.fetchChampions();
+		})
+	},
+
+	methods: {
+		fetchChampions: function() {
+			this.$http.get('/data/champions').then(function(response) {
+				console.log(response);
+				this.champions = response.body;
+			}, function(response) {
+				console.log(response);
+			});
+		}
 	}
-})
+
+});
