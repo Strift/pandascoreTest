3a3a3b0dffee8c6212a36a2569a930af020ebf15
Laurent <lau.cazanove@gmail.com>
2017-02-08 21:18:26 +0100

    Home route returns home page

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-var express = require('express');
-var app = express();
+/*
+ * Modules
+ */
+var express	= require('express');
+var fs 		= require('fs');
+var path 	= require('path');
 
+/*
+ * Configuration
+ */
 const port = 2020;
 
+/*
+ * Data
+ */
+const data = JSON.parse(fs.readFileSync('DATA.json'));
+
+/*
+ * Application
+ */
+var app = express();
+
+// Index
 app.get('/', function(req, res) {
 	res.send('Hello world');
 });
 
+// Home page
+app.get('/home', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/home.html'));
+});
+
+// Start server
 app.listen(port, function() {
 	console.log('App listening on port ' + port);
 });
